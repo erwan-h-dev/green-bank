2c410aea96f8c2d415e032212a1b8a78aca6b9fe
erwan dev <erwan.h.dev@gmail.com>
2023-11-20 22:09:42 +0100

    add simulation CRUD

---
+++ src/lib/types/simulation.ts
@@ -0,0 +1,38 @@
+export type Simulation = {
+    id?: number;
+    type: number;
+    energie: number;
+    kilometrage: number;
+    annee: number;
+    passagers: number;
+}
+
+let SIMULATIONS: Simulation[] = [];
+
+export function getSimulations(): Simulation[] 
+{
+    return SIMULATIONS;
+}
+
+export function getSimulation(id: number): Simulation|undefined 
+{
+    console.log(SIMULATIONS.find((simulation) => { simulation.id == id }))
+    return SIMULATIONS.find((simulation) => {simulation.id == id});
+}
+
+export function addSimulation(newSimulation: Simulation): Simulation {
+    // récupere l'id le plus grand de la collection
+    const maxId = SIMULATIONS.reduce((maxId, simulation) => (simulation.id && simulation.id > maxId ? simulation.id : maxId), 0);
+
+    // on incrémente la valeur de 1
+    newSimulation.id = maxId + 1;
+
+    SIMULATIONS.push(newSimulation);
+
+    return newSimulation;
+}
+
+export function deleteSimulation(id: number): void 
+{
+    SIMULATIONS = SIMULATIONS.filter((simulation) => simulation.id != id);
+}
