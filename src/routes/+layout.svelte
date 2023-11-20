9b68987898f02eae4efbf9621f3a3dbe10ee8141
erwan dev <erwan.h.dev@gmail.com>
2023-11-20 15:10:07 +0100

    First init + global layout & landing page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import '../app.postcss';
+    import { page } from '$app/stores';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+
+	// Floating UI for Popups
+	import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom';
+	import { storePopup } from '@skeletonlabs/skeleton';
+	storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Simulateur d'emprunt</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+                <a 
+                    href="/"  
+                    class="btn hover:variant-soft-primary {$page.url.pathname === '/' ? 'variant-soft-primary' : ''}"
+                >Accueil
+                </a>
+                <a 
+                    href="/simulations/new" 
+                    class="btn hover:variant-soft-primary {$page.url.pathname === '/simulations/new' ? 'variant-soft-primary' : ''}"
+                >Créer une simulation
+                </a>
+                <a 
+                    href="/simulations" 
+                    class="btn hover:variant-soft-primary {$page.url.pathname === '/simulations' ? 'variant-soft-primary' : ''}"
+                >Vos simulations
+                </a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+
+	<!-- Page Route Content -->
+    <main class="card w-100 m-3 p-3 variant-ghost">
+        <slot />
+    </main>
+    
+    <svelte:fragment slot="pageFooter">
+        <AppBar>
+            <div class="text-center text-sm">
+                <p>HOELTZENER Erwan 2023</p>
+            </div>
+        </AppBar>
+    </svelte:fragment>
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+
+<div class="container mx-auto">
+    
+    <h1 class="h1">Bienvenue sur l'Outil de Calcul de l'Empreinte Écologique de la Green Bank</h1>
+    
+    <div class="card variant-soft p-4 m-4">
+        <div class="p-4">
+            <p class="p-4">La Green Bank, fondée en 2022, est une institution financière novatrice axée sur la durabilité et l'impact environnemental positif. Son objectif principal est de catalyser des investissements et des initiatives qui favorisent la transition vers une économie verte et durable.</p>
+
+            <p class="p-4">Répondant à la nécessité croissante de mesurer et de comprendre l'impact environnemental des activités humaines, la Green Bank souhaite lancer un outil de calcul de l'empreinte écologique.</p>
+
+            <h2 class="h2 py-4">Comment ça marche ?</h2>
+
+            <a class="btn bg-gradient-to-br variant-gradient-warning-success color-white m-2" href="/simulation">Créer une simulation</a>
+
+            <p class="p-4">Remplissez le formulaire ci-dessous avec des informations sur votre mode de vie quotidien. Notre calculateur vous fournira une estimation de votre impact sur l'environnement.</p>
+
+            <p class="p-4">Une fois le formulaire rempli, cliquez sur le bouton de calcul pour obtenir vos résultats. Découvrez comment vos choix quotidiens affectent l'environnement et explorez des moyens de réduire votre empreinte écologique.</p>
+        </div>
+    </div>
+</div>
