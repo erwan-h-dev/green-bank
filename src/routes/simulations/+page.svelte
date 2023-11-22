1a439ee63cc1b460214a2290ff036a0dee341b6d
erwan dev <erwan.h.dev@gmail.com>
2023-11-22 17:39:07 +0100

    simulation page table + pagination

--- src/routes/simulations/+page.svelte
+++ src/routes/simulations/+page.svelte
@@ -1,24 +1,46 @@
 <script lang="ts">
-    import { CodeBlock, Table, tableMapperValues, type TableSource } from '@skeletonlabs/skeleton';
+    import { Table, tableMapperValues, type PaginationSettings, Paginator, tableSourceMapper } from '@skeletonlabs/skeleton';
     import type { PageData } from './$types';
 	import type { Simulation } from '$lib/models/simulation';
+	import { getNote } from '$lib/data/simulationService';
 
     export let data: PageData;
 
     const simulations = data.simulations;
 
+    // mise en place du jeu de données pour le tableau
+    let tableData: any[] = []
+    simulations.forEach((simulation) =>{
+        tableData.push({
+            id: simulation.id,
+            type: simulation.type.label,
+            energie: simulation.energie.label,
+            kilometrage: simulation.kilometrage.label,
+            annee: simulation.annee.label,
+            passagers: simulation.passagers.label,
+            note: getNote(simulation)
+        })
+    })
 
-    $:  getNote = (simulation: Simulation) => {
-        let totalNote = 0
+    // mise en place de la pagination
+    let paginationSettings = {
+        page: 0,
+        limit: 5,
+        size: tableData.length,
+        amounts: [5,10,25,100],
+    } satisfies PaginationSettings;
 
-        totalNote += simulation.type.note
-        totalNote += simulation.energie.note
-        totalNote += simulation.kilometrage.note
-        totalNote += simulation.annee.note
-
-        return totalNote
+    function selectSimulation(event: CustomEvent) {
+        window.location.href = `/simulations/${event.detail.id}`
     }
 
+    // gestion de la pagination du tableau
+    $: simulationsPagined = tableData.slice(
+        paginationSettings.page * paginationSettings.limit,
+        paginationSettings.page * paginationSettings.limit + paginationSettings.limit
+    );
+
+    const tableHead: string[] = ['N°','type','énergie','kilometrage','année','passagers','note']
 </script>
 
 <div class="container mx-auto">
@@ -26,35 +48,15 @@
     <h1 class="h1">Liste de vos simulations </h1>
 
     <div class="card variant-soft p-4 m-4">
-
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th>N°</th>
-                    <th>type</th>
-                    <th>energie</th>
-                    <th>kilometrage</th>
-                    <th>annee</th>
-                    <th>passagers</th>
-                    <th>Note</th>
-                </tr>
-            </thead>
-            <tbody>
-                {#each simulations as simulation}
-                    <tr class="list">
-                        <td><span class="badge bg-secondary-500 ms">{simulation.id}</span></td>
-                        <td>{simulation.type.label}</td>
-                        <td>{simulation.energie.label}</td>
-                        <td>{simulation.kilometrage.label}</td>
-                        <td>{simulation.annee.label}</td>
-                        <td>{simulation.passagers.label}</td>
-                        <td>{getNote(simulation)} / 40</td>
-                    </tr>
-                {/each}
-            </tbody>
-        </table>
-         
-                
-
+        <Table source={{
+                head: tableHead, 
+                body: tableMapperValues(simulationsPagined,  ['id','type','energie','kilometrage','annee','passagers','note']), 
+                meta: tableSourceMapper(simulationsPagined,  ['id']),
+                foot: tableHead
+            }}
+            interactive={true} 
+            on:selected={selectSimulation}
+        />
+        <Paginator class="my-3" bind:settings={paginationSettings} showFirstLastButtons="{false}" showPreviousNextButtons="{true}" showNumerals maxNumerals={1}></Paginator>
     </div>
 </div> 
